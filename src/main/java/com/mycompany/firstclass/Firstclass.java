34dcb30d8e7d7011146c987e8b57b76932ee8660
Manas162005 <manasm@sheridancollege.ca>
2025-09-15 19:43:07 -0400

    update name

--- src/main/java/com/mycompany/firstclass/Firstclass.java
+++ src/main/java/com/mycompany/firstclass/Firstclass.java
@@ -11,6 +11,6 @@ package com.mycompany.firstclass;
 public class Firstclass {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Manas");
     }
 }
