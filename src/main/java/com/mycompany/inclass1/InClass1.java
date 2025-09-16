1a376de1855a80c7d8303952969e01f6cd8c184c
Manas <manasm@sheridancollege.ca>
2025-09-16 00:22:51 -0400

    add dfg

---
+++ src/main/java/com/mycompany/inclass1/InClass1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.inclass1;
+
+/**
+ *
+ * @author manas
+ */
+public class InClass1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
